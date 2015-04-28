ddbd4941c1eee0ea3d60348b2cfbd719e8e9e634
Akshay <akshay.pushparaja@gmail.com>
2015-04-28 01:30:03 -0400

    missionSelector screen prettify

--- dicesterTest.spritebuilder/MissionSelector.m
+++ dicesterTest.spritebuilder/MissionSelector.m
@@ -14,16 +14,33 @@
     CCButton* _getEvenGameButton;
     CCButton* _oddBallGameButton;
     CCButton* _primeConcernGameButton;
+    CCLabelTTF* _getEvenLocked;
+    CCLabelTTF* _oddBallLocked;
+    CCLabelTTF* _primeConcernLocked;
     DiceManager* myDice;
 }
 
 -(void) didLoadFromCCB {
     self.userInteractionEnabled = TRUE;
     _getEvenGameButton.visible = false;
+    _oddBallGameButton.visible = false;
+    _primeConcernGameButton.visible = false;
+    _getEvenLocked.visible = true;
+    _oddBallLocked.visible = true;
+    _primeConcernLocked.visible = true;
     myDice = [DiceManager sharedDice];
     
     if ([myDice GetHighScore] > 5) {
+        _oddBallGameButton.visible = true;
+        _oddBallLocked.visible = false;
+    }
+    if ([myDice GetHighScore] > 10) {
         _getEvenGameButton.visible = true;
+        _getEvenLocked.visible = false;
+    }
+    if ([myDice GetHighScore] > 15) {
+        _primeConcernGameButton.visible = true;
+        _primeConcernLocked.visible = false;
     }
 }
 
